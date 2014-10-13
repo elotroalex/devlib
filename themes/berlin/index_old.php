39d3c836f24c03c2da6ebc557954db2dff5e945d
Meredith Levin <mjl2209@columbia.edu>
2014-10-13 11:19:43 -0400

    stashed the previous version of the index

--- themes/berlin/index_old.php
+++ themes/berlin/index_old.php
@@ -1,31 +1,37 @@
-<?php echo head(array('bodyid'=>'home', 'bodyclass' =>'two-col')); ?>
-<div id="primary">
+<html> 
+<head> 
+<title> DevLib </title>
+<link rel="stylesheet" type="text/css" href="themes/berlin/css/style.css">
+</head>
+<body>
+echo "<div style='text-align:center'><h1>Morningside Heights: A Digital History (1820-1950)</h1>
+<!-- Commented out old index file -->
+<!--<div id="primary">
     <?php if ($homepageText = get_theme_option('Homepage Text')): ?>
     <p><?php echo $homepageText; ?></p>
     <?php endif; ?>
-    <?php if (get_theme_option('Display Featured Item') == 1): ?>
-    <!-- Featured Item -->
+      <?php if (get_theme_option('Display Featured Item') == 1): ?>
+   Featured Item 
     <div id="featured-item">
         <h2><?php echo __('Featured Item'); ?></h2>
         <?php echo random_featured_items(1); ?>
-    </div><!--end featured-item-->
-    <?php endif; ?>
+    </div> end featured-item
+    <?php endif; ?> 
     <?php if (get_theme_option('Display Featured Collection')): ?>
-    <!-- Featured Collection -->
+    
     <div id="featured-collection">
         <h2><?php echo __('Featured Collection'); ?></h2>
         <?php echo random_featured_collection(); ?>
-    </div><!-- end featured collection -->
     <?php endif; ?>
     <?php if ((get_theme_option('Display Featured Exhibit')) && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
-    <!-- Featured Exhibit -->
+   
     <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
-    <?php endif; ?>
+    <?php endif; ?> 
 
-</div><!-- end primary -->
+</div>
 
 <div id="secondary">
-    <!-- Recent Items -->
+    
     <div id="recent-items">
         <h2><?php echo __('Recently Added Items'); ?></h2>
         <?php
@@ -47,9 +53,9 @@
         <p><?php echo __('No recent items available.'); ?></p>
         <?php endif; ?>
         <p class="view-items-link"><?php echo link_to_items_browse(__('View All Items')); ?></p>
-    </div><!-- end recent-items -->
+    </div> -->
     
     <?php fire_plugin_hook('public_home', array('view' => $this)); ?>
 
 </div><!-- end secondary -->
-<?php echo foot(); ?>
+</body>
