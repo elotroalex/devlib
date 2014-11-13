3389af118ff6cd3999bdb0beb3b8352400e709b6
Alex Gil <colibri.alex@gmail.com>
2014-11-13 12:45:30 -0500

    manually created HTML head/body

--- themes/berlin/index.php
+++ themes/berlin/index.php
@@ -1,4 +1,17 @@
-<?php echo head(array('bodyid'=>'home', 'bodyclass' =>'two-col')); ?>
-<div id="primary">Empty box</div>
+<head>
+    <meta charset="utf-8">
+
+       
+    
+        <title>My DevLib</title>
+
+
+        <!-- Stylesheets -->
+    <link href="http://localhost:8888/devlib/themes/berlin/css/style.css" media="all" rel="stylesheet" type="text/css" >
+    
+    </head>
+    
+<body>
+<div id="primary">Empty box</div></body>
 
 
