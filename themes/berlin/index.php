ec58405576ea6be9863333d33f0f7d0dbfb6c6d7
Alex Gil <colibri.alex@gmail.com>
2014-11-13 12:50:00 -0500

    added id primary-home

--- themes/berlin/index.php
+++ themes/berlin/index.php
@@ -1,17 +1,13 @@
 <head>
-    <meta charset="utf-8">
-
-       
+    <meta charset="utf-8">     
     
         <title>My DevLib</title>
-
-
         <!-- Stylesheets -->
     <link href="http://localhost:8888/devlib/themes/berlin/css/style.css" media="all" rel="stylesheet" type="text/css" >
-    
-    </head>
+</head>
     
 <body>
-<div id="primary">Empty box</div></body>
+<div id="primary-home">Empty box</div>
+</body>
 
 
