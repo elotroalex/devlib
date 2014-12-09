727caf0e818a77183616590df769a747f745f8fd
Alex Gil <colibri.alex@gmail.com>
2014-12-09 15:02:32 -0500

    made menu text white and pushed the margin a bit

--- themes/berlin/index.php
+++ themes/berlin/index.php
@@ -8,7 +8,7 @@
     
 <body>
 <div id="primary-home"><img id="logo" src="files/images/mhdh-logo.png" alt="Smiley face"  width="100%">
-<p>Empty box</p>
+<p id="homepage-menu">Empty box</p>
 </div>
 </body>
 
